7160e12b48749fa81c5916bc844043e56731a088
P10r10 <asantiago@keemail.me>
2023-07-11 11:41:48 +0100

    hockey statistics

--- hockey_statistics.py
+++ hockey_statistics.py
@@ -1,16 +1,21 @@
-class FileReader:
-    def __init__(self, file_name) -> None:
-        pass
+import json
 
-    def read_data(self) -> dict:
-        pass  # TODO
+
+class FileReader:
+
+    @staticmethod
+    def read_data() -> dict:
+        # file_name = input("file name: ")
+        file_name = "partial.json"  # REMOVE
+        with open(file_name) as fp:
+            return json.loads(fp.read())
 
 
 class HockeyApp:
-    def __init__(self) -> None:
-        file_name = input("file name: ")
-        self.__players = FileReader(file_name).read_data()
+    def __init__(self, players: list[dict]) -> None:
+        self.__players = players
         self.__menu()
+        self.__run()
 
     def __menu(self):
         print("""
@@ -25,6 +30,43 @@ commands:
 7 most goals
               """)
 
+    def __run(self):
+        while True:
+            command = input("command: ")
+            if command == "0":
+                break
+            self.__process(command)
+
+    def __process(self, command: str):
+        if command == "1":
+            self.__search_for_player()
+        elif command == "2":
+            self.__teams()
+        elif command == "3":
+            self.__countries()
+
+    def __search_for_player(self):
+        name = input("name: ")
+        try:
+            p = [p for p in self.__players if p["name"] == name][0]  # player
+            print(f"{p['name']:21}{p['team']:5}{p['goals']:2} + "
+                  f"{p['assists']:2} = {p['goals'] + p['assists']:3}")
+        except:
+            pass
+
+    def __teams(self):
+        teams = sorted(list(set([p["team"] for p in self.__players])))
+        for team in teams:
+            print(team)
+
+    def __countries(self):
+        countries = sorted(
+            list(set([p["nationality"] for p in self.__players])))
+        for country in countries:
+            print(country)
+
 
 if __name__ == "__main__":
-    HockeyApp()
+    players = FileReader().read_data()
+    print(f"read the data of {len(players)} players")
+    HockeyApp(players)
